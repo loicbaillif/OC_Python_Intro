3c435642cfdf70a56331523f9d2464b8ff810c6f
LoicBaillif <larmedeciel@gmail.com>
2020-02-21 12:37:30 +0100

    Chapter 2 Finished

---
+++ chapi2/cours.py
@@ -0,0 +1,25 @@
+laurene_phone_number = "0760147648"
+print ("Téléphone de Laurène: ")
+print (laurene_phone_number)
+laurene_phone_number = "0673626082"
+print ("Téléphone de Laurène: " + laurene_phone_number)
+del(laurene_phone_number)
+print("Le numéro de téléphone de Laurène a été effacé")
+print(1 < 1)
+print(2>1)
+laurene_contact = ["Laurène", "Sans-Nom", "0673626082", "Laon"]
+print(laurene_contact)
+print("Téléphone de Laurène: " + laurene_contact[2])
+print("Type tuple:")
+Paris = (48.856578, 2.351828)
+print("Coordonnées de Paris: ")
+print(Paris)
+print("Type dictionnaire:")
+my_dictionary = {"un": "one", "deux": "two", "trois": "three"}
+print(my_dictionary["un"])
+print("""Euh ça fonctionne vraiment ça?
+oui?
+Non?
+""")
+print(type(my_dictionary))
+print(type(laurene_contact))
